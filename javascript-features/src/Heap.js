8ad10914e097cae913b58ac621fc9b8a555b0dd0
Aragor70 <mikis3@o2.pl>
2020-12-16 00:57:24 +0000

    Initial commit, heap algorithm

---
+++ javascript-features/src/Heap.js
@@ -0,0 +1,58 @@
+
+// get Heap's algorithms
+
+
+// function getPermutations(1, 2, 3)
+// function getPermutations(A, 1, C, 2, 3)
+// generate result "321,312,231,213,132,123"
+
+// output returns numbers without letters, from highest to lowest value
+
+
+
+
+const getPermutations = ( ...arry ) => {
+
+    const convert = (array) => {
+
+        return array[0].split(', ').filter(element => element * 1)
+
+    }
+
+    const output = []
+    const swapThePosition = ( arrayToSwap, first, second ) => {
+        const positionOfFirst = arrayToSwap[first]
+        arrayToSwap[first] = arrayToSwap[second]
+        arrayToSwap[second] = positionOfFirst
+    }
+
+    const generate = (n, featureArray) => {
+
+        if (n == 1) {
+            return output.push(featureArray.slice())
+        }
+
+        generate(n - 1, featureArray)
+
+        for (let i = 0; i < n - 1; i++) {
+            if (n % 2 == 0) {
+                swapThePosition(featureArray, i, n - 1)
+            } else {
+                swapThePosition(featureArray, 0, n - 1)
+            }
+            generate(n - 1, featureArray)
+
+        }
+        
+    }
+    
+    generate(convert(arry).length, convert(arry).slice())
+
+
+    const joinValues = output.map(arr => arr.join('') * 1).sort((a, b) => b - a)
+
+    return joinValues.join(',')
+
+}
+
+console.log(getPermutations( '1, A, 2, 3' ))
---
+++ readMe.md
@@ -0,0 +1,17 @@
+## Programming algorithms
+
+This section is dedicated to return algorithms, the most examples will be provide in JavaScript and TypeScript languages.
+Please note me any qestions for other languages recommendation.
+
+
+This section helps you with the exams, interviews or exploring cases of your own curiosity I have been faced with.
+
+I encourage you to send your feedback.
+
+---
+* version 1.0.0
+* created by mikey.prus@gmail.com
+
+
+### JavaScript:
+Heap algorithm
